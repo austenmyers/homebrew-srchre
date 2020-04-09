c03ab296a62d4259bcfb2c356fa5da2c6c2e88e9
austenmyers <austenmy@usc.edu>
2020-04-09 13:53:12 -0700

    update sh command trailing '

--- srchre.rb
+++ srchre.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Srchre < Formula
   desc "A CLI tool to search specific credible sites and QA sites for the purpose of a one query research tool for google search results."
-  homepage "https://github.com/austenmyers/srchre"
+  homepage "https://github.com/austenmyers/homebrew-srchre"
   url "https://github.com/austenmyers/srchre/archive/1.0.tar.gz"
   sha256 "7d91bf939aae4ca498027ba500eef976cadce46163541d971f2c35bf9cab7c4b"
 
@@ -10,6 +10,6 @@ class Srchre < Formula
   depends_on "googler" => :recommended
 
   def install
-    bin.install 'srchre'
+    bin.install "srchre"
   end
 end
