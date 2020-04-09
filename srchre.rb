b19de3071dd9955563943236a540d9a0f86f44bf
austenmyers <austenmy@usc.edu>
2020-04-09 13:56:09 -0700

    update tar ball for new update

--- srchre.rb
+++ srchre.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Srchre < Formula
   desc "A CLI tool to search specific credible sites and QA sites for the purpose of a one query research tool for google search results."
   homepage "https://github.com/austenmyers/homebrew-srchre"
-  url "https://github.com/austenmyers/srchre/archive/1.0.tar.gz"
+  url "https://github.com/austenmyers/homebrew-srchre/archive/1.1.tar.gz"
   sha256 "7d91bf939aae4ca498027ba500eef976cadce46163541d971f2c35bf9cab7c4b"
 
   depends_on "bash-completion" => :recommended
