936d66d6d4041ea7d39d39d10a7e3cd7fae9f546
austenmyers <austenmy@usc.edu>
2020-04-09 14:10:27 -0700

    update sha256 for tar 1.1

--- srchre.rb
+++ srchre.rb
@@ -4,6 +4,7 @@ class Srchre < Formula
   desc "A CLI tool to search specific credible sites and QA sites for the purpose of a one query research tool for google search results."
   homepage "https://github.com/austenmyers/homebrew-srchre"
   url "https://github.com/austenmyers/homebrew-srchre/archive/1.1.tar.gz"
+  sha256 "199ae0cf3fd9fdf1d59529d96267590db97e039d1d23caf70400d64511b15e5b"
 
   depends_on "bash-completion" => :recommended
   depends_on "googler" => :recommended
